f0b4cb261304e0913d2b58baf92a1996264dbbfa
Javeane <Javeanew@gmail.com>
2023-02-24 20:42:31 +0800

    My Wordpress Plugin

--- includes/ajax-handler.php
+++ includes/ajax-handler.php
@@ -1,110 +1,71 @@
 <?php
 
 /**
- * This file is responsible for handling all Ajax requests
+ * This file is responsible for handling AJAX requests
  *
  * @package My_Wordpress_Plugin
  */
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+    exit;
 }
 
 /**
- * Handle Ajax requests
+ * Handle avatar upload AJAX request
  */
-function my_wp_plugin_ajax_handler() {
+function my_wp_plugin_handle_avatar_upload() {
+    $response = array();
 
-	// Check if request is an Ajax request
-	if ( ! isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) || strtolower( $_SERVER['HTTP_X_REQUESTED_WITH'] ) != 'xmlhttprequest' ) {
-		return;
-	}
+    // Verify nonce
+    if ( ! wp_verify_nonce( $_POST['nonce'], 'my-wp-plugin-avatar-upload' ) ) {
+        $response['success'] = false;
+        $response['message'] = __( 'Nonce verification failed', 'my-wp-plugin' );
+        wp_send_json( $response );
+    }
 
-	// Check the action parameter and call the corresponding function
-	if ( isset( $_POST['action'] ) ) {
+    // Check if a file was uploaded
+    if ( ! isset( $_FILES['file'] ) ) {
+        $response['success'] = false;
+        $response['message'] = __( 'No file uploaded', 'my-wp-plugin' );
+        wp_send_json( $response );
+    }
 
-		switch ( $_POST['action'] ) {
+    // Get the uploaded file
+    $file = $_FILES['file'];
 
-			case 'my_wp_plugin_upload_avatar':
-				my_wp_plugin_upload_avatar();
-				break;
+    // Check for errors
+    if ( $file['error'] !== UPLOAD_ERR_OK ) {
+        $response['success'] = false;
+        $response['message'] = __( 'File upload error', 'my-wp-plugin' );
+        wp_send_json( $response );
+    }
 
-			case 'my_wp_plugin_delete_avatar':
-				my_wp_plugin_delete_avatar();
-				break;
+    // Generate a unique file name
+    $file_name = uniqid() . '_' . $file['name'];
 
-			default:
-				wp_send_json_error( 'Invalid action' );
-				break;
-		}
+    // Move the uploaded file to the plugin's upload directory
+    $upload_dir = MY_WP_PLUGIN_UPLOAD_DIR;
+    if ( ! file_exists( $upload_dir ) ) {
+        mkdir( $upload_dir, 0755, true );
+    }
+    $file_path = $upload_dir . '/' . $file_name;
+    if ( ! move_uploaded_file( $file['tmp_name'], $file_path ) ) {
+        $response['success'] = false;
+        $response['message'] = __( 'Error saving uploaded file', 'my-wp-plugin' );
+        wp_send_json( $response );
+    }
 
-	} else {
-		wp_send_json_error( 'No action specified' );
-	}
+    // Get the user ID
+    $user_id = get_current_user_id();
 
-	exit;
+    // Update the user's meta data
+    update_user_meta( $user_id, 'my_wp_plugin_avatar', $file_path );
+
+    // Return the file URL to the frontend
+    $response['success'] = true;
+    $response['file_url'] = MY_WP_PLUGIN_UPLOAD_URL . '/' . $file_name;
+    wp_send_json( $response );
 }
-add_action( 'wp_ajax_my_wp_plugin_ajax_handler', 'my_wp_plugin_ajax_handler' );
-add_action( 'wp_ajax_nopriv_my_wp_plugin_ajax_handler', 'my_wp_plugin_ajax_handler' );
-
-/**
- * Upload user avatar
- */
-function my_wp_plugin_upload_avatar() {
-
-	// Check if user is logged in
-	if ( ! is_user_logged_in() ) {
-		wp_send_json_error( 'You must be logged in to upload an avatar' );
-	}
-
-	// Check nonce
-	if ( ! wp_verify_nonce( $_POST['nonce'], 'my_wp_plugin_upload_avatar' ) ) {
-		wp_send_json_error( 'Invalid nonce' );
-	}
-
-	// Check if file was uploaded
-	if ( ! isset( $_FILES['avatar_file'] ) || ! $_FILES['avatar_file']['error'] == UPLOAD_ERR_OK ) {
-		wp_send_json_error( 'No file uploaded' );
-	}
-
-	// Get the current user's ID
-	$user_id = get_current_user_id();
-
-	// Get the file extension
-	$file_extension = strtolower( pathinfo( $_FILES['avatar_file']['name'], PATHINFO_EXTENSION ) );
-
-	// Check if file extension is allowed
-	$allowed_extensions = array( 'jpg', 'jpeg', 'png', 'gif' );
-	if ( ! in_array( $file_extension, $allowed_extensions ) ) {
-		wp_send_json_error( 'Invalid file type' );
-	}
-
-	// Generate a unique file name
-	$file_name = uniqid( 'avatar_' . $user_id . '_' ) . '.' . $file_extension;
-
-	// Upload the file
-	$upload_dir = wp_upload_dir();
-	$upload_path = $upload_dir['basedir'] . '/my_wp_plugin_avatars/';
-	if ( ! file_exists( $upload_path ) ) {
-		mkdir( $upload_path, 0755, true );
-	}
-	if ( ! move_uploaded_file( $_FILES['avatar_file']['tmp_name'], $upload_path . $file_name ) ) {
-		wp_send_json_error( 'Failed to upload file' );
-	}
-
-		// Update the user's meta data
-	update_user_meta( $user_id, 'my_wp_plugin_avatar', $file_name );
-
-	// Create response object
-	$response = array(
-		'success' => true,
-		'message' => __( 'Avatar uploaded successfully.', 'my-wp-plugin' ),
-		'url'     => $upload_dir['baseurl'] . '/' . $file_name,
-	);
-
-	// Send the JSON response
-	wp_send_json( $response );
-}
-add_action( 'wp_ajax_my_wp_plugin_upload_avatar', 'my_wp_plugin_upload_avatar' );
-add_action( 'wp_ajax_nopriv_my_wp_plugin_upload_avatar', 'my_wp_plugin_upload_avatar' );
+add_action( 'wp_ajax_my_wp_plugin_avatar_upload', 'my_wp_plugin_handle_avatar_upload' );
+add_action( 'wp_ajax_nopriv_my_wp_plugin_avatar_upload', 'my_wp_plugin_handle_avatar_upload' );
